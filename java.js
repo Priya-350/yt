f8dbea0869763c5e36186488b763b54db9c06e5b
PRIYADHARSHINI <151350953+Priya-350@users.noreply.github.com>
2024-06-22 17:18:04 +0530

    Add files via upload

---
+++ java.js
@@ -0,0 +1,11 @@
+function nav(){
+    var home=document.getElementById("m");
+    var img=document.getElementById("im");
+    var img1=document.getElementById("em");
+    home.classList.toggle("misuse");
+    img.classList.toggle("c");
+    img1.classList.toggle("c");
+
+    
+   
+}
